e8777edbef7c7c7ff319b0f07be8c43bc9b8ebeb
nato63 <natorono273@gmail.com>
2026-08-09 06:42:18 +0300

    a commit with all the added componets and the new updated app.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+function About({
+  image = "https://via.placeholder.com/215",
+  about = "About this blog",
+}) {
+  return (
+    <aside>
+      <img src={image} alt="blog logo" />
+      <p>{about}</p>
+    </aside>
+  );
+}
+
+export default About;
